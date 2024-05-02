de4c91deaf808b88ca73eeaecfc04be8d0d7f6ee
hieudm.tt <hieudm.tt@mcredit.com.vn>
2024-05-02 14:04:26 +0700

    bug kh nhan thu vien bootstrap

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,39 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.hoidanit.laptopshop.service.UserService;
+
+//@RestController
+//public class UserController {
+//    private UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/")
+//    public String getHomePage(){
+//        return this.userService.handleHello();
+//    }
+//}
+@Controller
+public class UserController{
+
+        private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @RequestMapping("/")
+    public String getHomePage(Model model){
+        String test = userService.handleHello();
+        model.addAttribute("fromService",test);
+        model.addAttribute("fromService2","Dinh Minh Hieu");
+//        return "eric.html";
+        return "hello";
+    }
+}
